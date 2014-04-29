184fc2d4e445d7bb8b61ffa62b1d3676f18d1b71
Marvin <washington_marv@yahoo.com>
2014-04-28 23:18:30 -0400

    First commit for C++ repo

---
+++ src/C++_HelloWorld.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : C++_HelloWorld.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Welcome to the new Eclipse" << endl;
+	return 0;
+}
